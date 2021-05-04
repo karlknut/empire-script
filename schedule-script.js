f442d5d0e2dd2d53c76cd55d2e486785f72f4acc
karlknut <karl-knut.kanemagi@tptlive.ee>
2021-05-04 21:37:58 +0300

    cleaned up, added logic and sendMail script

--- schedule-script.js
+++ schedule-script.js
@@ -2,9 +2,18 @@
 
 const schedule = require('node-schedule');
 const empireMain = require('./empire-html');
+const notification = require('./push-notification');
+const sendMail = require('./email-notification');
 
 
-const job = schedule.scheduleJob('*/30 * * * * *', () => { // runs every 30 seconds.
-  empireMain();
+const job = schedule.scheduleJob('*/30 * * * * *', async () => { // runs every 30 seconds.
+  console.log('checking dice');
+  const diceCount = await empireMain();
+  console.log(diceCount);
+  if (diceCount <= 3) {
+    console.log('sending message');
+    notification();
+    sendMail();
+  };
 });
 
