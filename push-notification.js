4546dedabf94c635e26ac53bcbb99ba1267a493d
karlknut <karl-knut.kanemagi@tptlive.ee>
2021-05-04 21:37:12 +0300

    fixed sending current dice count

--- push-notification.js
+++ push-notification.js
@@ -1,15 +1,20 @@
 // Send WhatsApp messages
 
-const diceCount = require('./empire-html');
-
+const empireMain = require('./empire-html');
 const wbm = require('wbm');
 
-wbm.start().then(async () => {
-    const contacts = [
-        { phone: '+3725526641', dice: diceCount },
-        { phone: '+37256578761', dice: diceCount }
-    ];
-    const message = 'Dice count is {{diceCount}}';
-    await wbm.send(contacts, message);
-    await wbm.end();
-}).catch(err => console.log(err));
+const notification = () => {
+    wbm.start().then(async () => {
+        const diceCount = await empireMain();
+        const contacts = [
+            { phone: '+3725526641', dice: diceCount },
+            { phone: '+37256578761', dice: diceCount }
+        ];
+        const message = 'Dice count is {{dice}}';
+        await wbm.send(contacts, message);
+        await wbm.end();
+    }).catch(err => console.log(err));
+};
+
+module.exports = notification;
+
